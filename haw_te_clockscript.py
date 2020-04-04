4bdf00ece49d9fc3a2c80e9ff572392c3fbc404b
Ante Skoric <44805084+anteskoric@users.noreply.github.com>
2020-04-04 18:50:55 +0200

    Update haw_te_clockscript.py

--- haw_te_clockscript.py
+++ haw_te_clockscript.py
@@ -1,25 +1,20 @@
 #Bibliotheken einbinden
 import RPi.GPIO as GPIO
 import time
-
 #GPIO Modus (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
-
 #Warnungen ausschalten
 GPIO.setwarnings(False)
-
 #GPIO Pin Belegung
 ROT = 2
 GELB = 14
 GRUEN = 15
-TASTER = 7
-
+TASTER = 21
 #Richtung der GPIO-Pins festlegen (IN / OUT)
 GPIO.setup(ROT, GPIO.OUT) #rot
 GPIO.setup(GELB, GPIO.OUT) #gelb
 GPIO.setup(GRUEN, GPIO.OUT) #gruen
 GPIO.setup(TASTER, GPIO.IN) #Taster
-
 #Umschaltung definieren
 def umschalten():
     #Phase 2
@@ -39,14 +34,12 @@ def umschalten():
     #zurueck zu Phase 1
     GPIO.output(ROT, True)
     GPIO.output(GELB, False)
-
 #Endlosschleife
 while True:
     #Phase 1
     GPIO.output(ROT, True)
     GPIO.output(GELB, False)
     GPIO.output(GRUEN, False)
-    
     #Status des Tasters einlesen
     tasterStatus = GPIO.input(TASTER)
     if (tasterStatus):
